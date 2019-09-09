5d127d06ab728f37a21ccebda39bc5189fe372f1
deha <root@localhost.localdomain>
2019-09-09 12:02:52 +0200

    Finished GA_toy.py

--- GeneticAlgorithms/GA_toy.py
+++ GeneticAlgorithms/GA_toy.py
@@ -1,22 +1,30 @@
 import numpy as np
 
+from queue import PriorityQueue
+
+
 class Gene():
     def __init__(self, gene_seed):
         if isinstance(gene_seed, int):
             self.genome = np.random.binomial(1, 0.5, gene_seed)
         else:
-            self.genome = gene_seed
+            self.genome = np.copy(gene_seed)
 
         self.gene_size = len(self.genome)
 
     def mutate(self):
-        """ YOUR CODE HERE!"""
-        raise NotImplementedError
+        num_bits = int(gene_size / 2)
+        part_to_mutate = self.genome[-num_bits:]
+        mutation = np.random.binomial(1, 0.5, num_bits)
+        new_genome = [*self.genome[:num_bits] *mutation]
+        self.genome = new_genome
 
     def evaluate_fitness(self, target):
         """ Lower fitness is better. Perfect fitness should equal 0"""
-        """ YOUR CODE HERE!"""
-        raise NotImplementedError
+        fitness = 0
+        for bit in self.genome:
+            fitness = (fitness << 1) | bit
+        return fitness
 
 
 class GeneticAlgorithm():
@@ -26,7 +34,7 @@ class GeneticAlgorithm():
         self.target = target
         self.parents = []
 
-        self.gene_pool = [[np.nan, Gene(self.gene_size)] for _ in range(self.pop_size )]
+        self.gene_pool = [[np.nan, Gene(self.gene_size)] for _ in range(self.pop_size)]
 
     def evaluate_population(self):
         """ Evaluates the fitness of ever genome. If the best fitness is 0
@@ -45,20 +53,33 @@ class GeneticAlgorithm():
 
     def select_parents(self, num_parents):
         """ Function that selects num_parents from the population."""
-        """ YOUR CODE HERE!"""
-        raise NotImplementedError
+        pq = PriorityQueue()
+        for gene in self.gene_pool:
+            fitness = gene[1].evaluate_fitness(self.target)
+            if pq.qsize() < num_parents:
+                pq.put(gene, -fitness)
+            else:
+                head = pq.get()
+                if fitness < head[0]:
+                    pq.put(gene, gene[0])
+                else:
+                    pq.put(head, head[0])
+        self.parents = list(pq.queue)
 
     def produce_next_generation(self):
         """ Function that creates the next generation based on parents."""
-        """ YOUR CODE HERE!"""
-        raise NotImplementedError
+        #next_gen = []
+        for parent in self.parents:
+            parent[1].mutate()
+        #    next_gen.append(mutated)
+        #self.gene_pool = next_gen
 
     def run(self):
         done = False
         i = 1
-        while not done :
+        while not done:
             done = self.evaluate_population()
-            self.select_parents(int(self.pop_size/10)+1)
+            self.select_parents(int(self.pop_size / 10) + 1)
 
             if i % 5 == 0 or done:
                 print("Generation:", i)
@@ -72,6 +93,7 @@ class GeneticAlgorithm():
 
             self.produce_next_generation()
             i += 1
+
 
 pop_size = 10
 gene_size = 20
